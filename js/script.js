2ea71c4aefad8141dae94c6c4d2ad924f3f344f2
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-06-15 21:20:06 -0300

    Exibição dos resultados de ângulos, perímetro e área

--- js/script.js
+++ js/script.js
@@ -11,16 +11,56 @@ let res = document.getElementById('res')
             if (existência == 'não existe') {
                 window.alert('Esses valores não podem formar um triângulo')
             } else {
+                //Cálculo dos ângulos via lei dos cossenos
+                
                 let angA = (180*Math.acos((l[1]**2 + l[3]**2 - l[2]**2)/(2*l[1]*l[3])))/Math.PI //lei dos cossenos + conversão de cosseno para ângulo + conversão para graus
                 
                 let angB = (180*Math.acos((l[1]**2 + l[2]**2 - l[3]**2)/(2*l[1]*l[2])))/Math.PI
 
                 let angC = (180*Math.acos((l[3]**2 + l[2]**2 - l[1]**2)/(2*l[3]*l[2])))/Math.PI
 
-                for(let c = 1;c<=7;c++) {
+                // Cálculo do perímetro
+
+                let perímetro = l[1] + l[2] + l[3]
+
+                //Cálculo da área
+
+                let área = l[1]*l[3]*(Math.sin(angA*(Math.PI)/180))/2
+
+                //Criação dos parágrafos que exibirão os resultados
+
+                for(let c = 1;c<=5;c++) {
                     let p = document.createElement('p')
+                    if (c<=3) {
+                        p.setAttribute('class','angulo') 
+                    } else if (c == 4) {
+                        p.setAttribute('id','perimetro')
+                    } else {
+                        p.setAttribute('id','area')
+                    }
+                    
                     res.appendChild(p)
                 }
+
+                //Resultado dos ângulos
+
+                resultadoAngulo = document.getElementsByClassName('angulo')
+
+                resultadoAngulo[0].innerHTML = `Ângulo do vértice A: ${angA}°`
+                resultadoAngulo[1].innerHTML = `Ângulo do vértice B: ${angB}°`
+                resultadoAngulo[2].innerHTML = `Ângulo do vértice C: ${angC}°`
+
+                //Resultado do perímetro
+
+                resultadoPerímetro = document.getElementById('perimetro')
+
+                resultadoPerímetro.innerHTML = `Perímetro: ${perímetro}`
+
+                //Resultado da área
+
+                resultadoÁrea = document.getElementById('area')
+
+                resultadoÁrea.innerHTML = `Área: ${área}`
 
                 
             }
